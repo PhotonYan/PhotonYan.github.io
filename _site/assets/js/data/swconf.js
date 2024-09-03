cc333643c93b102321027ea3867a1189619e0162
Photon <photonyan@stu.pku.edu.cn>
2024-09-04 07:54:19 +0800

    fuck

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1725406529',resources: [
+    cacheName: 'chirpy-1725407638',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
