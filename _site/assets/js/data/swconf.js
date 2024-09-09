52a975b31e6e33d57540514b90c2b0d564f72f35
Photon <photonyan@stu.pku.edu.cn>
2024-09-10 00:14:48 +0800

    11111111

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1725897857',resources: [
+    cacheName: 'chirpy-1725898457',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
